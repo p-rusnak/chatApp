ce4e1b343990c6f2c39c4e565edc1c625d61feb0
Paweł <snaki009@gmail.com>
2017-07-30 12:44:18 +0200

    Initial setup
    Basic setup for react with express

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.set("views", __dirname + '/views');
+app.set("view engine", 'ejs');
+
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+app.listen(3000, function(){
+  console.log("Server started");
+});
